248a0f119b96306f3f812c187416b0f0242b2f2f
Jack Liston <jack.liston4@mail.dcu.ie>
2019-10-23 17:08:21 +0100

    test

--- HackerClub-Calculator.py
+++ HackerClub-Calculator.py
@@ -2,6 +2,7 @@
 def main():
   print(addition(3,5))
   print(subtraction(4,2))
+  print(multi(3,3))
   print("Finished")
   
 def addition(a,b):
@@ -11,6 +12,8 @@ def subtraction(a,b):
 	min = a - b
 	return min
 	
+def multi(a,b):
+   return a * b
 if __name__ == '__main__':
 	main()
 
